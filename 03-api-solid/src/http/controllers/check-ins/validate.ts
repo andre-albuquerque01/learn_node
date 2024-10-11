f5545d53fe11fd3aa012765f6d185ee1f17f59fe
andre <andrealbuquerque2001@gmail.com>
2024-10-11 11:00:15 -0300

    feat: create

---
+++ 03-api-solid/src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,18 @@
+import { makeCheckInService } from "@/services/factories/make-check-ins-service"
+import { makeValidateCheckInService } from "@/services/factories/make-validate-check-in-service"
+import { FastifyReply, FastifyRequest } from "fastify"
+import { z } from "zod"
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+    const validateCheckInParamsSchema = z.object({
+        checkInId: z.string().uuid(),
+    })
+
+    const { checkInId } = validateCheckInParamsSchema.parse(request.params)
+
+    const validateCheckInService = makeValidateCheckInService()
+
+    await validateCheckInService.execute({ checkInId })
+
+    return reply.status(204).send()
+} 
