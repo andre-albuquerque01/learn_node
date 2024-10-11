e7050c0db1e9c989fb257759feadf917264495aa
andre <andrealbuquerque2001@gmail.com>
2024-10-11 10:59:30 -0300

    feat: create service

---
+++ 03-api-solid/src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+import { FastifyInstance } from "fastify"
+import request from "supertest"
+
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            role: isAdmin ? 'ADMIN' : 'MEMBER',
+            password_hash: await hash('1234567', 6),
+        },
+    })
+    const authResponse = await request(app.server)
+        .post('/sessions')
+        .send({
+            email: 'johndoe@example.com',
+            password: '1234567',
+        })
+
+    const { token } = authResponse.body
+
+    return { token }
+}
