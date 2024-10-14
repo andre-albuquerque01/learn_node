62996091b7a0581ed5ce16c49e7cda0102fb3c93
andre <andrealbuquerque2001@gmail.com>
2024-10-14 10:49:44 -0300

    feat: create user-case

---
+++ 04-clean-ddd/src/domain/user-cases/answer-question.ts
@@ -0,0 +1,27 @@
+import { Answer } from "../entities/answer"
+import { AnswersRepository } from "../repositories/answers-repository"
+
+interface AnswerQuestionUseCaseRequest {
+    questionId: string
+    instructorId: string
+    content: string
+}
+
+export class AnswerQuestionUseCase {
+
+    constructor(
+        private answerRepository: AnswersRepository 
+    ){}
+
+    async execute({ questionId, instructorId, content }: AnswerQuestionUseCaseRequest) {
+        const answer = new Answer({
+            content,
+            authorId: instructorId,
+            questionId
+        })
+
+        await this.answerRepository.create(answer)
+
+        return answer
+    }
+}
