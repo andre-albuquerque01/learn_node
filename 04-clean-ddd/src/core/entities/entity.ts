8d07a43dcb794392378f417aaa3f8a60f498f647
andre <andrealbuquerque2001@gmail.com>
2024-10-17 10:31:16 -0300

    feat: update

---
+++ 04-clean-ddd/src/core/entities/entity.ts
@@ -0,0 +1,16 @@
+import { UniqueEntityID } from './unique-entity-id'
+
+export class Entity<Props> {
+  private _id: UniqueEntityID
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected props: any
+
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: Props, id?: UniqueEntityID) {
+    this.props = props
+    this._id = id ?? new UniqueEntityID()
+  }
+}
