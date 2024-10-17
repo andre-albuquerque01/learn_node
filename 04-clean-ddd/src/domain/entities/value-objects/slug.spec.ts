b6efeb396dc2f4cd23ebc87492aaabf35ee400d3
andre <andrealbuquerque2001@gmail.com>
2024-10-17 10:31:59 -0300

    feat: update

--- 04-clean-ddd/src/domain/entities/value-objects/slug.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug";
-
-test('it should be able to create a new slug from text', () => {
-    const slug = Slug.createFromText('Example create questio title')
-
-    expect(slug.value).toEqual('example-create-questio-title')
-})
