9cea25913e82ae171cbfe2a7d46bbff06ede7f11
Andre <andrealbuquerque2001@gmail.com>
2024-10-18 10:20:21 -0300

    feat: create

---
+++ 04-clean-ddd/src/domain/forum/application/user-cases/answer-question.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async () => {},
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '2',
+    content: 'Nova resposta',
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
