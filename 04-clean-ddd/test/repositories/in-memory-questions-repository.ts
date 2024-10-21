b3cbc20c2fe9476305bfac6067e88ce0b6942fcc
andre <andrealbuquerque2001@gmail.com>
2024-10-21 11:32:03 -0300

    feat: create

---
+++ 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+export class InMemoryQuestionsRepository implements QuestionRepository {
+    public items: Question[] = []
+
+    async findById(id: string) {
+        const question = this.items.find(item => item.id.toString() === id)
+        if (!question) return null
+        return question
+    }
+
+    async findBySlug(slug: string) {
+        const question = this.items.find(item => item.slug.value === slug)
+        if (!question) return null
+        return question
+    }
+
+    async create(question: Question) {
+        this.items.push(question)
+    }
+
+    async save(question: Question) {
+        const itemIndex = this.items.findIndex(item => item.id === question.id)
+        this.items[itemIndex] = question
+    }
+
+    async delete(question: Question) {
+        const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+        this.items.splice(itemIndex, 1)
+    }
+}
