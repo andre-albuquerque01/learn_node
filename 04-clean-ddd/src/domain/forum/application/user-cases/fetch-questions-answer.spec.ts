4e4aa35e719bdb01764ba81505e5a9e089dfaa03
andre <andrealbuquerque2001@gmail.com>
2024-10-23 10:28:51 -0300

    feat: create

---
+++ 04-clean-ddd/src/domain/forum/application/user-cases/fetch-questions-answer.spec.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { expect, it, beforeEach } from 'vitest'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { FetchQuestionsAnswerUseCase } from './fetch-questions-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: FetchQuestionsAnswerUseCase
+
+describe('Fetch question answers', () => {
+    beforeEach(() => {
+        inMemoryAnswersRepository = new InMemoryAnswersRepository()
+        sut = new FetchQuestionsAnswerUseCase(inMemoryAnswersRepository)
+    })
+
+    it('should be able to fecth question answers', async () => {
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+
+        const { answers } = await sut.execute({
+            questionId: '1',
+            page: 1
+        })
+
+        expect(answers).toHaveLength(3)
+    })
+
+    it('should be able to fecth paginated question answers', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        }
+
+        const { answers } = await sut.execute({
+            questionId: '1',
+            page: 2
+        })
+
+        expect(answers).toHaveLength(2)
+    })
+})
