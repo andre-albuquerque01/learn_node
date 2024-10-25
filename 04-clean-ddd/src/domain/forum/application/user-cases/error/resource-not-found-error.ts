3ee2ecbf4b5c0ca74d1bcec5090af4fee96ebfac
andre <andrealbuquerque2001@gmail.com>
2024-10-25 09:49:57 -0300

    feat: create

---
+++ 04-clean-ddd/src/domain/forum/application/user-cases/error/resource-not-found-error.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class ResourceNotFoundError extends Error implements UseCaseError {
+  constructor() {
+    super('Resource not found')
+  }
+}
