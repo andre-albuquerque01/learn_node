b2b952890b978583fbf944e606ac536dbea5684a
Andre <andrealbuquerque2001@gmail.com>
2024-10-26 10:20:21 -0300

    feat: update

--- 04-clean-ddd/src/domain/forum/application/user-cases/fetch-questions-answer.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/user-cases/fetch-questions-answer.spec.ts
@@ -19,12 +19,12 @@ describe('Fetch question answers', () => {
         await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
         await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
 
-        const { answers } = await sut.execute({
+        const result = await sut.execute({
             questionId: '1',
             page: 1
         })
 
-        expect(answers).toHaveLength(3)
+        expect(result.value?.answers).toHaveLength(3)
     })
 
     it('should be able to fecth paginated question answers', async () => {
@@ -32,11 +32,11 @@ describe('Fetch question answers', () => {
             await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
         }
 
-        const { answers } = await sut.execute({
+        const result = await sut.execute({
             questionId: '1',
             page: 2
         })
 
-        expect(answers).toHaveLength(2)
+        expect(result.value?.answers).toHaveLength(2)
     })
 })
