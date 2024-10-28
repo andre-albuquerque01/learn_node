cb03318c7a0e39600b42c0b9f6819da2524ccab3
Andre <andrealbuquerque2001@gmail.com>
2024-10-28 10:40:21 -0300

    feat: update

--- 04-clean-ddd/src/domain/forum/application/user-cases/edit-answer.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/user-cases/edit-answer.spec.ts
@@ -4,6 +4,7 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-r
 import { makeAnswer } from 'test/factories/make-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { EditAnswerUseCase } from './edit-answer'
+import { NotAllowError } from './error/not-allow-error'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let sut: EditAnswerUseCase
@@ -39,21 +40,13 @@ describe('Edit answer', () => {
 
         await inMemoryAnswersRepository.create(newAnswer)
 
-        expect(() => {
-            return sut.execute({
-                questionId: newAnswer.id.toValue(),
-                authorId: '2',
-                content: 'UUUUUU'
-            })
-        }).rejects.toThrow('Unauthorized to edit this answer')
-
-        expect(() => {
-            return sut.execute({
-                questionId: newAnswer.id.toValue(),
-                authorId: '2',
-                content: 'UUUUUU'
-            })
-        }).rejects.toBeInstanceOf(Error)
-
+        const result = await sut.execute({
+            questionId: newAnswer.id.toValue(),
+            authorId: '2',
+            content: 'UUUUUU'
+        })
+        
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowError)
     })
 })
