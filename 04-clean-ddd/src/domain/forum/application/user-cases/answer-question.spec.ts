4695b67bbcaf2490e5795f9eb74471aa890ce7ad
Andre <andrealbuquerque2001@gmail.com>
2024-10-29 10:40:21 -0300

    feat: update

--- 04-clean-ddd/src/domain/forum/application/user-cases/answer-question.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/user-cases/answer-question.spec.ts
@@ -12,13 +12,13 @@ describe('create anwer', () => {
   })
 
   it('should be able to create an answer', async () => {
-    const { answer } = await sut.execute({
+    const result = await sut.execute({
       questionId: '1',
       instructorId: '2',
       content: 'Nova resposta',
     })
 
-    expect(answer.content).toBeTruthy()
-    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryAnswersRepository.items[0]).toEqual(result.value?.answer)
   })
 })
