934326bac5dd3e03fffe65f0af94ba47467e2eb9
Andre <andrealbuquerque2001@gmail.com>
2024-10-31 09:40:21 -0300

    feat: update

--- 04-clean-ddd/src/domain/forum/application/user-cases/answer-question.ts
+++ 04-clean-ddd/src/domain/forum/application/user-cases/answer-question.ts
@@ -2,11 +2,14 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '../../enterprise/entities/answer'
 import { Either, right } from '@/core/either'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 interface AnswerQuestionUseCaseRequest {
   questionId: string
   instructorId: string
   content: string
+  attachmentsIds: string[]
 }
 
 type AnswerQuestionUseCaseResponse = Either<null, { answer: Answer }>
@@ -17,12 +20,22 @@ export class AnswerQuestionUseCase {
     questionId,
     instructorId,
     content,
+    attachmentsIds,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
     })
+
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachment: new UniqueEntityID(attachmentId),
+        answerId: answer.id,
+      })
+    })
+
+    answer.attachments = new AnswerAttachmentList(answerAttachments)
 
     await this.answerRepository.create(answer)
 
