265fe99ab7650459ee80d7feac360b3941cfc192
Andre <andrealbuquerque2001@gmail.com>
2024-10-31 09:40:21 -0300

    feat: update

--- 04-clean-ddd/src/domain/forum/application/user-cases/create-question.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/user-cases/create-question.spec.ts
@@ -1,13 +1,20 @@
 import { expect, it, beforeEach } from 'vitest'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionsAttachmentRepository } from 'test/repositories/in-memory-questions-attachment-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionsAttachmentRepository: InMemoryQuestionsAttachmentRepository
 let sut: CreateQuestionUseCase
 
 describe('create a question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionsAttachmentRepository =
+      new InMemoryQuestionsAttachmentRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionsAttachmentRepository,
+    )
     sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
@@ -16,9 +23,19 @@ describe('create a question', () => {
       authorId: '1',
       title: 'New question',
       content: "question's content",
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({ attachment: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachment: new UniqueEntityID('2') }),
+    ])
   })
 })
