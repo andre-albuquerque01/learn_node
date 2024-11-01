f5fadbd8c12b9f6bb21ec87026bde74fa03021f6
Andre <andrealbuquerque2001@gmail.com>
2024-11-01 09:40:21 -0300

    feat: create

---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { WatchedList } from '@/core/entities/watched-list'
+import { QuestionAttachment } from './question-attachment'
+
+export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
+  compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
+    return a.attachment === b.attachment
+  }
+}
