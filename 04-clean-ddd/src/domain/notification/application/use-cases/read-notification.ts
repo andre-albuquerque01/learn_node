7f934e0a235f79cf71863f4c16ef0d8dfd7d9cba
Andre <andrealbuquerque2001@gmail.com>
2024-11-02 08:40:21 -0300

    feat: create

---
+++ 04-clean-ddd/src/domain/notification/application/use-cases/read-notification.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import { Either, left, right } from '@/core/either'
+import { Notification } from '../../enterprise/entities/notification'
+import { NotificationRepository } from '../repositories/notification-repository'
+import { ResourceNotFoundError } from '@/core/errors/error/resource-not-found-error'
+import { NotAllowError } from '@/core/errors/error/not-allow-error'
+
+interface ReadNotificationUseCaseRequest {
+  recipientId: string
+  notificationId: string
+}
+
+type ReadNotificationUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowError,
+  {
+    notification: Notification
+  }
+>
+
+export class ReadNotificationUseCase {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute({
+    recipientId,
+    notificationId,
+  }: ReadNotificationUseCaseRequest): Promise<ReadNotificationUseCaseResponse> {
+    const notification =
+      await this.notificationRepository.findById(notificationId)
+
+    if (!notification) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (recipientId !== notification.recipientId.toString()) {
+      return left(new NotAllowError())
+    }
+
+    notification.read()
+
+    await this.notificationRepository.save(notification)
+
+    return right({ notification })
+  }
+}
