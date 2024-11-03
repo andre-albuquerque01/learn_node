ffa71edf90494ccc7c71892057eee51af6bf6cd5
Andre <andrealbuquerque2001@gmail.com>
2024-11-03 08:40:21 -0300

    feat: create

--- 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-questions-repository.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentRepository } from '@/domain/forum/application/repositories/question-attachment-repository'
 import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
@@ -31,11 +32,13 @@ export class InMemoryQuestionsRepository implements QuestionRepository {
 
     async create(question: Question) {
         this.items.push(question)
+        DomainEvents.dispatchEventsForAggregate(question.id)
     }
 
     async save(question: Question) {
         const itemIndex = this.items.findIndex(item => item.id === question.id)
         this.items[itemIndex] = question
+        DomainEvents.dispatchEventsForAggregate(question.id)
     }
 
     async delete(question: Question) {
