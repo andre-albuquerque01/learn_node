ac1d4f6c946d258c77cbb9ad00d21b8302ceb4ea
Andre <andrealbuquerque2001@gmail.com>
2024-11-07 08:05:21 -0300

    feat: update

---
+++ 04-clean-ddd/src/core/errors/error/not-allow-error.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class NotAllowError extends Error implements UseCaseError {
+  constructor() {
+    super('Not allowed')
+  }
+}
