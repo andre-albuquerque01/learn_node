528409d403a9a99026a5f8c18d33bf0db964fe20
Andre <andrealbuquerque2001@gmail.com>
2024-11-14 08:25:41 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/repositories/question-attachment-repository.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
+
+export interface QuestionAttachmentRepository {
+  findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
+  deleteManyByQuestionId(questionId: string): Promise<void>
+}
