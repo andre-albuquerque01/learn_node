b2fa78139673b0233274fb046d2d73b0ee49eee5
Andre <andrealbuquerque2001@gmail.com>
2024-11-16 09:52:32 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/user-cases/delete-answer.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import { Either, left, right } from '@/core/either'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { NotAllowError } from '@/core/errors/error/not-allow-error'
+import { ResourceNotFoundError } from '@/core/errors/error/resource-not-found-error'
+
+interface DeleteAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+type DeleteAnswerUseCaseResponse  = Either<ResourceNotFoundError | NotAllowError, null>
+
+export class DeleteAnswerUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      return left(new NotAllowError())
+    }
+
+    await this.answerRepository.delete(answer)
+
+    return right(null)
+  }
+}
