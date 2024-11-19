efc5b40c1473ecb447d4e1431d290fa18f8f5e06
Andre <andrealbuquerque2001@gmail.com>
2024-11-19 09:15:12 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/enterprise/entities/question-attachment.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface QuestionAttachmentProps {
+    questionId: UniqueEntityID
+    attachment: UniqueEntityID
+}
+
+export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
+    get questionId() {
+        return this.props.questionId
+    }
+
+    get attachment() {
+        return this.props.attachment
+    }
+
+    static create(props: QuestionAttachmentProps, id?: UniqueEntityID) {
+        const attachment = new QuestionAttachment(props, id)
+        return attachment
+    }
+}
