8ce2b68ea36860a09cb38f2bbde425381b785736
Andre <andrealbuquerque2001@gmail.com>
2024-11-21 09:35:16 -0300

    feat: create

---
+++ 05-nestJs/src/infra/database/database.module.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: QuestionRepository,
+      useClass: PrismaQuestionsRepository
+    },
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [PrismaService,
+    QuestionRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,],
+})
+export class DatabaseModule {}
