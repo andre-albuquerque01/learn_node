dac562c2f15c652a8f73cc4ce3d8ac17b20bb8c7
Andre <andrealbuquerque2001@gmail.com>
2024-11-24 08:10:03 -0300

    feat: create

---
+++ 05-nestJs/test/utils/wait-for.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+export async function waitFor(
+  assertions: () => void,
+  maxDuration = 1000,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let elapsedTime = 0
+
+    const interval = setInterval(() => {
+      elapsedTime += 10
+
+      try {
+        assertions()
+        clearInterval(interval)
+        resolve()
+      } catch (err) {
+        if (elapsedTime >= maxDuration) {
+          reject(new Error('Timeout exceeded'))
+        }
+      }
+    }, 10)
+  })
+}
