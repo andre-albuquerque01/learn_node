287c83cd9363a9491405644a6d12c401f61f0060
Andre <andrealbuquerque2001@gmail.com>
2024-11-26 08:10:03 -0300

    feat: update

---
+++ 05-nestJs/src/domain/forum/application/repositories/student-repository.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import { Student } from '../../enterprise/entities/student'
+
+export abstract class StudentRepository {
+  abstract findByEmail(email: string): Promise<Student | null>
+  abstract create(student: Student): Promise<void>
+}
