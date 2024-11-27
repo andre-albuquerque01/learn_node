64a3897bcefb9b459d84ccdcdcdf83a2516d855c
Andre <andrealbuquerque2001@gmail.com>
2024-11-27 09:19:03 -0300

    feat: update

--- 05-nestJs/src/domain/forum/application/user-cases/fetch-question-comment.ts
+++ 05-nestJs/src/domain/forum/application/user-cases/fetch-question-comment.ts
@@ -2,6 +2,7 @@
 import { Either, right } from '@/core/either'
 import { QuestionComment } from '../../enterprise/entities/question-comments'
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+import { Injectable } from '@nestjs/common'
 
 interface FetchQuestionCommentsUseCaseRequest {
   questionId: string
@@ -12,6 +13,7 @@ type FetchQuestionCommentsUseCaseResponse = Either<null, {
   questionsComment: QuestionComment[]
 }>
 
+@Injectable()
 export class FetchQuestionCommentsUseCase {
   constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
 
