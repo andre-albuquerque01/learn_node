28127ba36dd5e8a84caa01c14814c1145f50be8e
Andre <andrealbuquerque2001@gmail.com>
2024-11-29 09:21:30 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/user-cases/errors/wrong-credentials-error.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class WrongCredentialsError extends Error implements UseCaseError {
+  constructor() {
+    super('Credentials are not valid.')
+  }
+}
