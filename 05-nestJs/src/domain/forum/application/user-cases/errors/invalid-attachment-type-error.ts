65aa3c4d68f26f0313ed152f53108d8cb4fad8d2
Andre <andrealbuquerque2001@gmail.com>
2024-11-29 09:21:30 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/user-cases/errors/invalid-attachment-type-error.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { UseCaseError } from '@/core/errors/use-case-error'
+
+export class InvalidAttachmentTypeError extends Error implements UseCaseError {
+  constructor(type: string) {
+    super(`File type "${type}" is not valid.`)
+  }
+}
