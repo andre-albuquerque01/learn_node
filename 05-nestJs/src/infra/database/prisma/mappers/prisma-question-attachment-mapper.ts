ff7e4833da00fb2a5f2c409b02c8a778eaab9ac8
Andre <andrealbuquerque2001@gmail.com>
2024-12-01 08:25:30 -0300

    feat: create

---
+++ 05-nestJs/src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+export class PrismaQuestionAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): QuestionAttachment {
+    if (!raw.questionId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return QuestionAttachment.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        attachment: new UniqueEntityID(raw.id),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
