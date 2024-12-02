f4a87a9a135486affb21ee9efcbbdb3671fba1ae
Andre <andrealbuquerque2001@gmail.com>
2024-12-02 10:02:30 -0300

    feat: update

--- 05-nestJs/src/infra/http/http.module.ts
+++ 05-nestJs/src/infra/http/http.module.ts
@@ -7,15 +7,79 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
 import { DatabaseModule } from '@faker-js/faker/.'
 import { CreateQuestionUseCase } from '@/domain/forum/application/user-cases/create-question'
 import { FetchRecentQuestionUseCase } from '@/domain/forum/application/user-cases/fecth-recent-questions'
+import { RegisterStudentUseCase } from '@/domain/forum/application/user-cases/register-student'
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/user-cases/authenticate-student'
+import { CryptographyModule } from '../cryptography/cryptography.module'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/user-cases/get-question-by-slug'
+import { EditQuestionController } from './controllers/edit-question.controller'
+import { EditQuestionUseCase } from '@/domain/forum/application/user-cases/edit-question'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/user-cases/delete-question'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/user-cases/answer-question'
+import { EditAnswerController } from './controllers/edit-answer.controller'
+import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { DeleteAnswerUseCase } from '@/domain/forum/application/user-cases/delete-answer'
+import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller'
+import { FetchQuestionsAnswerUseCase } from '@/domain/forum/application/user-cases/fetch-questions-answer'
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/user-cases/choose-questin-best-answer'
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/user-cases/comment-on-question'
+import { DeleteQuestionCommentController } from './controllers/delete-question-comment.controller'
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/user-cases/delete-question-comment'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/user-cases/comment-on-answer'
+import { CommentOnAnswerController } from './controllers/comment-on-answer.controller'
+import { DeleteAnswerCommentController } from './controllers/delete-answer-comment.controller'
+import { FetchQuestionCommentsController } from './controllers/fetch-question-comments.controller'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/user-cases/fetch-question-comment'
+import { FetchAnswerCommentsController } from './controllers/fetch-answer-comments.controller'
+import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/user-cases/fetch-answer-comment'
+import { UploadAttachmentController } from './controllers/upload-attachment.controller'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
+    EditQuestionController,
+    DeleteQuestionController,
+    AnswerQuestionController,
+    EditAnswerController,
+    DeleteAnswerController,
+    FetchQuestionAnswersController,
+    ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
+    DeleteQuestionCommentController,
+    CommentOnAnswerController,
+    DeleteAnswerCommentController,
+    FetchQuestionCommentsController,
+    FetchAnswerCommentsController,
+    UploadAttachmentController,
   ],
-  providers: [CreateQuestionUseCase, FetchRecentQuestionUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase,
+    EditQuestionUseCase,
+    DeleteQuestionUseCase,
+    AnswerQuestionUseCase,
+    EditQuestionUseCase,
+    DeleteAnswerUseCase,
+    FetchQuestionsAnswerUseCase,
+    ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
+    DeleteQuestionCommentUseCase,
+    CommentOnAnswerUseCase,
+    DeleteAnswerUseCase,
+    FetchQuestionCommentsUseCase,
+    FetchAnswerCommentsUseCase,
+  ],
 })
 export class HttpModule {}
