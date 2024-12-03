53ecec588158facf697d76a58a265c244306f967
Andre <andrealbuquerque2001@gmail.com>
2024-12-03 09:58:52 -0300

    feat: create

---
+++ 05-nestJs/src/infra/env/env.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET_PRIVATE: z.string(),
+  JWT_SECRET_PUBLIC: z.string(),
+  PORT: z.coerce.number().optional().default(3333),
+})
+
+export type Env = z.infer<typeof envSchema>
