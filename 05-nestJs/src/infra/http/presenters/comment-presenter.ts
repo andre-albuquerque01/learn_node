598b9df8fa5f86a6c5aaf546005b200f2f129dc7
Andre <andrealbuquerque2001@gmail.com>
2024-12-06 09:12:58 -0300

    feat: create

---
+++ 05-nestJs/src/infra/http/presenters/comment-presenter.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Comment } from '@/domain/forum/enterprise/entities/comment'
+
+export class CommentPresenter {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static toHTTP(comment: Comment<any>) {
+    return {
+      id: comment.id.toString(),
+      content: comment.content,
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt,
+    }
+  }
+}
