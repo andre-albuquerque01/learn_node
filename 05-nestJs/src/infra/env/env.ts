6dad9cfae12ce1d862729641873f7534256f47f6
Andre <andrealbuquerque2001@gmail.com>
2024-12-09 09:52:16 -0300

    feat: update

--- 05-nestJs/src/infra/env/env.ts
+++ 05-nestJs/src/infra/env/env.ts
@@ -5,6 +5,11 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET_PRIVATE: z.string(),
   JWT_SECRET_PUBLIC: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_BUCKET_KEY_ID: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
 })
 
