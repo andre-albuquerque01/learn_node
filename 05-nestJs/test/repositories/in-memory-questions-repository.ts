d6350f9210a5b3de01e3a18eae9f2b24fa985f5a
Andre <andrealbuquerque2001@gmail.com>
2024-12-10 09:52:16 -0300

    feat: update

--- 05-nestJs/test/repositories/in-memory-questions-repository.ts
+++ 05-nestJs/test/repositories/in-memory-questions-repository.ts
@@ -6,44 +6,61 @@ import { QuestionRepository } from '@/domain/forum/application/repositories/ques
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionRepository {
-    public items: Question[] = []
+  public items: Question[] = []
 
-    constructor(private questionAttachmentRepository: QuestionAttachmentRepository){}
+  constructor(
+    private questionAttachmentRepository: QuestionAttachmentRepository,
+  ) {}
 
-    async findById(id: string) {
-        const question = this.items.find(item => item.id.toString() === id)
-        if (!question) return null
-        return question
-    }
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+    if (!question) return null
+    return question
+  }
 
-    async findBySlug(slug: string) {
-        const question = this.items.find(item => item.slug.value === slug)
-        if (!question) return null
-        return question
-    }
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+    if (!question) return null
+    return question
+  }
 
-    async findManyRecent({ page }: PaginationParams) {
-        const questions = this.items
-            .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-            .slice((page - 1) * 20, page * 20)
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
 
-        return questions
-    }
+    return questions
+  }
 
-    async create(question: Question) {
-        this.items.push(question)
-        DomainEvents.dispatchEventsForAggregate(question.id)
-    }
+  async create(question: Question) {
+    this.items.push(question)
 
-    async save(question: Question) {
-        const itemIndex = this.items.findIndex(item => item.id === question.id)
-        this.items[itemIndex] = question
-        DomainEvents.dispatchEventsForAggregate(question.id)
-    }
+    await this.questionAttachmentRepository.createMany(
+      question.attachments.getItems(),
+    )
 
-    async delete(question: Question) {
-        const itemIndex = this.items.findIndex(item => item.id === question.id)
-        this.items.splice(itemIndex, 1)
-        this.questionAttachmentRepository.deleteManyByQuestionId(question.id.toString())
-    }
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items[itemIndex] = question
+
+    await this.questionAttachmentRepository.createMany(
+      question.attachments.getNewItems(),
+    )
+    await this.questionAttachmentRepository.deleteMany(
+      question.attachments.getRemovedItems(),
+    )
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items.splice(itemIndex, 1)
+    this.questionAttachmentRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
+  }
 }
