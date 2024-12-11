e2e6d0e6b5f11b405a3879ecbe934314e14ca76a
Andre <andrealbuquerque2001@gmail.com>
2024-12-11 09:59:16 -0300

    feat: update

--- 05-nestJs/src/domain/forum/application/user-cases/comment-on-question.spec.ts
+++ 05-nestJs/src/domain/forum/application/user-cases/comment-on-question.spec.ts
@@ -5,31 +5,48 @@ import { makeQuestion } from 'test/factories/make-question'
 import { InMemoryQuestionsCommentsRepository } from 'test/repositories/in-memory-questions-comments-repository'
 import { CommentOnQuestionUseCase } from './comment-on-question'
 import { InMemoryQuestionsAttachmentRepository } from 'test/repositories/in-memory-questions-attachment-repository'
+import { InMemoryStudentRepository } from 'test/repositories/in-memory-student-repository'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryQuestionsCommentsRepository: InMemoryQuestionsCommentsRepository
 let inMemoryQuestionsAttachmentRepository: InMemoryQuestionsAttachmentRepository
+let inMemoryStudentRepository: InMemoryStudentRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
 let sut: CommentOnQuestionUseCase
 
 describe('Comment on question', () => {
-    beforeEach(() => {
-        inMemoryQuestionsAttachmentRepository = new InMemoryQuestionsAttachmentRepository()
-        inMemoryQuestionsRepository = new InMemoryQuestionsRepository(inMemoryQuestionsAttachmentRepository)
-        inMemoryQuestionsCommentsRepository = new InMemoryQuestionsCommentsRepository()
-        sut = new CommentOnQuestionUseCase(inMemoryQuestionsRepository, inMemoryQuestionsCommentsRepository)
+  beforeEach(() => {
+    inMemoryQuestionsAttachmentRepository =
+      new InMemoryQuestionsAttachmentRepository()
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    inMemoryStudentRepository = new InMemoryStudentRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionsAttachmentRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentRepository,
+    )
+    inMemoryQuestionsCommentsRepository =
+      new InMemoryQuestionsCommentsRepository(inMemoryStudentRepository)
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionsCommentsRepository,
+    )
+  })
+
+  it('should be able to comment question', async () => {
+    const newQuestion = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: newQuestion.authorId.toString(),
+      content: 'Hello World!',
     })
 
-    it('should be able to comment question', async () => {
-        const newQuestion = makeQuestion()
-
-        await inMemoryQuestionsRepository.create(newQuestion)
-
-        await sut.execute({
-            questionId: newQuestion.id.toString(),
-            authorId: newQuestion.authorId.toString(),
-            content: 'Hello World!',
-        })
-
-        expect(inMemoryQuestionsCommentsRepository.items[0].content).toEqual('Hello World!')
-    })
+    expect(inMemoryQuestionsCommentsRepository.items[0].content).toEqual(
+      'Hello World!',
+    )
+  })
 })
