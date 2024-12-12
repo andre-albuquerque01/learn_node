b51b9655c4d8481e2c84737624b0ddbd73a2fa04
Andre <andrealbuquerque2001@gmail.com>
2024-12-12 10:13:28 -0300

    feat: update

--- 05-nestJs/src/domain/forum/application/user-cases/get-question-by-slug.spec.ts
+++ 05-nestJs/src/domain/forum/application/user-cases/get-question-by-slug.spec.ts
@@ -5,34 +5,70 @@ import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
 import { InMemoryQuestionsAttachmentRepository } from 'test/repositories/in-memory-questions-attachment-repository'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { InMemoryStudentRepository } from 'test/repositories/in-memory-student-repository'
+import { makeStudent } from 'test/factories/make-student'
+import { makeAttachment } from 'test/factories/make-attachment'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryQuestionsAttachmentRepository: InMemoryQuestionsAttachmentRepository
+let inMemoryAttachmentRepository: InMemoryAttachmentsRepository
+let inMemoryStudentRepository: InMemoryStudentRepository
 let sut: GetQuestionBySlugUseCase
 
 describe('Get question by slug', () => {
-    beforeEach(() => {
-        inMemoryQuestionsAttachmentRepository = new InMemoryQuestionsAttachmentRepository()
-        inMemoryQuestionsRepository = new InMemoryQuestionsRepository(inMemoryQuestionsAttachmentRepository)
-        sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  beforeEach(() => {
+    inMemoryQuestionsAttachmentRepository =
+      new InMemoryQuestionsAttachmentRepository()
+    inMemoryAttachmentRepository = new InMemoryAttachmentsRepository()
+    inMemoryStudentRepository = new InMemoryStudentRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionsAttachmentRepository,
+      inMemoryAttachmentRepository,
+      inMemoryStudentRepository,
+    )
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const student = makeStudent({ name: 'John doe' })
+    inMemoryStudentRepository.items.push(student)
+
+    const newQuestion = makeQuestion({
+      authorId: student.id,
+      slug: Slug.create('slug'),
     })
 
-    it('should be able to get a question by slug', async () => {
-        const newQuestion = makeQuestion({
-            slug: Slug.create('slug'),
-        })
+    await inMemoryQuestionsRepository.create(newQuestion)
 
-        await inMemoryQuestionsRepository.create(newQuestion)
-
-        const result = await sut.execute({
-            slug: 'slug',
-        })
-
-        expect(result.isRight()).toBe(true)
-        expect(result.value).toMatchObject({
-            question: expect.objectContaining({
-                title: newQuestion.title,
-            })
-        })
+    const attachment = makeAttachment({
+      title: 'Some attachment',
     })
+    inMemoryAttachmentRepository.items.push(attachment)
+
+    inMemoryQuestionsAttachmentRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachment: attachment.id,
+      }),
+    )
+
+    const result = await sut.execute({
+      slug: 'slug',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toMatchObject({
+      question: expect.objectContaining({
+        title: newQuestion.title,
+        author: 'John doe',
+        attachments: expect.arrayContaining([
+          expect.objectContaining({
+            title: 'Some attachment',
+          }),
+        ]),
+      }),
+    })
+  })
 })
