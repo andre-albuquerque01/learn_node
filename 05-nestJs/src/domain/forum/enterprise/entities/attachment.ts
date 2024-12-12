198677a581857d9d40db912e49d95be5e9b34557
Andre <andrealbuquerque2001@gmail.com>
2024-12-12 10:13:28 -0300

    feat: update

--- 05-nestJs/src/domain/forum/enterprise/entities/attachment.ts
+++ 05-nestJs/src/domain/forum/enterprise/entities/attachment.ts
@@ -2,7 +2,7 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface AttachmentProps {
+export interface AttachmentProps {
     title: string
     url: string
 }
