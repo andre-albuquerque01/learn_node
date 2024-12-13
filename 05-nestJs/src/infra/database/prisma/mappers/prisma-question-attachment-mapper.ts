a6cd65608e74bf27fe3b1e5e7e905f850978b413
Andre <andrealbuquerque2001@gmail.com>
2024-12-13 08:59:58 -0300

    feat: update

--- 05-nestJs/src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ 05-nestJs/src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
-import { Attachment as PrismaAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 
 export class PrismaQuestionAttachmentMapper {
   static toDomain(raw: PrismaAttachment): QuestionAttachment {
@@ -16,5 +16,22 @@ export class PrismaQuestionAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachment.toString()
+    })
+
+    return {
+      where: {
+        id: { in: attachmentIds },
+      },
+      data: {
+        questionId: attachments[0].questionId.toString(),
+      },
+    }
   }
 }
