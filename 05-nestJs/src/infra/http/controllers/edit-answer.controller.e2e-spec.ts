4ae5d2b5440cb8a3835a201d803a632cf42f8822
Andre <andrealbuquerque2001@gmail.com>
2024-12-15 07:58:26 -0300

    feat: update

--- 05-nestJs/src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
+++ 05-nestJs/src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -7,6 +7,8 @@ import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
 import { AnswerFactory } from 'test/factories/make-answer'
+import { AnswerAttachmentFactory } from 'test/factories/make-answer-attachment'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
@@ -17,17 +19,21 @@ describe('Choose question best answer (E2E)', () => {
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let answerFactory: AnswerFactory
+  let attachmentFactory: AttachmentFactory
+  let answerAttachmentFactory: AnswerAttachmentFactory
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, AnswerFactory, QuestionFactory],
+      providers: [StudentFactory, AnswerFactory, QuestionFactory, AttachmentFactory, AnswerAttachmentFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
     studentFactory = moduleRef.get(StudentFactory)
     answerFactory = moduleRef.get(AnswerFactory)
     questionFactory = moduleRef.get(QuestionFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    answerAttachmentFactory = moduleRef.get(AnswerAttachmentFactory)
     prisma = moduleRef.get(PrismaService)
     jwt = moduleRef.get(JwtService)
 
@@ -48,21 +54,57 @@ describe('Choose question best answer (E2E)', () => {
       authorId: user.id,
     })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+    
     const answerId = answer.id.toString()
+    
+    await answerAttachmentFactory.makePrismaAttachment({
+      attachment: attachment1.id,
+      answerId: answer.id
+    })
+    await answerAttachmentFactory.makePrismaAttachment({
+      attachment: attachment2.id,
+      answerId: answer.id
+    })
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment()
 
     const response = await request(app.getHttpServer())
       .patch(`/answers/${answerId}/choose-as-best`)
       .set('Authorization', `Bearer ${accessToken}`)
-      .send()
+      .send({
+        content: 'New answer content',
+        attachments: [
+          attachment1.id.toString(),
+          attachment3.id.toString(),
+        ]
+      })
 
     expect(response.status).toBe(204)
 
-    const questionOnDatabase = await prisma.question.findUnique({
+    const answerOnDatabase = await prisma.answer.findUnique({
       where: {
-        id: question.id.toString()
+        id: answer.id.toString()
       },
     })
 
-    expect(questionOnDatabase?.bestAnswerId).toEqual(answerId)
+    expect(answerOnDatabase).toEqual(answerId)
+
+    const attachmentOnDatabase = await prisma.attachment.findMany({
+      where: {
+        answerId: answerOnDatabase?.id
+      },
+    })
+
+    expect(attachmentOnDatabase.length).toBe(2)
+    expect(attachmentOnDatabase).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        id: attachment1.id.toString(),
+      }),
+      expect.objectContaining({
+        id: attachment3.id.toString(),
+      }),
+    ]))
   })
 })
