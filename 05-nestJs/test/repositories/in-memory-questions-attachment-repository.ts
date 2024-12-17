5031204dc06fe12530f31d6080764634248b8bd9
Andre <andrealbuquerque2001@gmail.com>
2024-12-17 10:52:16 -0300

    feat: update

--- 05-nestJs/test/repositories/in-memory-questions-attachment-repository.ts
+++ 05-nestJs/test/repositories/in-memory-questions-attachment-repository.ts
@@ -2,18 +2,36 @@
 import { QuestionAttachmentRepository } from '@/domain/forum/application/repositories/question-attachment-repository'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
-export class InMemoryQuestionsAttachmentRepository implements QuestionAttachmentRepository {
-    public items: QuestionAttachment[] = []
-    
-    async findManyByQuestionId(questionId: string) {
-        const questionAttachment = this.items
-        .filter(item => item.questionId.toString() === questionId)
-        
-        return questionAttachment
-    }
-    
-    async deleteManyByQuestionId(questionId: string) {
-        const questionAttachment = this.items = this.items.filter(item => item.questionId.toString() !== questionId)
-        this.items = questionAttachment 
-    }
+export class InMemoryQuestionsAttachmentRepository
+  implements QuestionAttachmentRepository
+{
+  public items: QuestionAttachment[] = []
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachment = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    )
+
+    return questionAttachment
+  }
+
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachment = (this.items = this.items.filter(
+        (item) => {
+            return !attachments.some((attachment) => attachment.equals(item))
+        }
+      ))
+      this.items = questionAttachment
+  }
+
+  async deleteManyByQuestionId(questionId: string) {
+    const questionAttachment = (this.items = this.items.filter(
+      (item) => item.questionId.toString() !== questionId,
+    ))
+    this.items = questionAttachment
+  }
 }
