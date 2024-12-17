fa19924b18c4c9655383d9c13b51b537a1fde3cb
Andre <andrealbuquerque2001@gmail.com>
2024-12-17 10:52:16 -0300

    feat: update

--- 05-nestJs/test/repositories/in-memory-answer-attachment-repository.ts
+++ 05-nestJs/test/repositories/in-memory-answer-attachment-repository.ts
@@ -2,18 +2,34 @@
 import { AnswerAttachmentRepository } from '@/domain/forum/application/repositories/answer-attachment-repository'
 import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
 
-export class InMemoryAnswersAttachmentRepository implements AnswerAttachmentRepository {
-    public items: AnswerAttachment[] = []
-    
-    async findManyByAnswerId(answerId: string) {
-        const answerAttachment = this.items
-        .filter(item => item.answerId.toString() === answerId)
-        
-        return answerAttachment
-    }
-    
-    async deleteManyByAnswerId(answerId: string) {
-        const answerAttachment = this.items = this.items.filter(item => item.answerId.toString() !== answerId)
-        this.items = answerAttachment 
-    }
+export class InMemoryAnswersAttachmentRepository
+  implements AnswerAttachmentRepository
+{
+  public items: AnswerAttachment[] = []
+
+  async createMany(attachments: AnswerAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
+    const answerAttachment = (this.items = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    }))
+    this.items = answerAttachment
+  }
+
+  async findManyByAnswerId(answerId: string) {
+    const answerAttachment = this.items.filter(
+      (item) => item.answerId.toString() === answerId,
+    )
+
+    return answerAttachment
+  }
+
+  async deleteManyByAnswerId(answerId: string) {
+    const answerAttachment = (this.items = this.items.filter(
+      (item) => item.answerId.toString() !== answerId,
+    ))
+    this.items = answerAttachment
+  }
 }
