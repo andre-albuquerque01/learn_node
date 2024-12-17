b7e80af0a3d6f665a7c927bb7f65c232df299105
Andre <andrealbuquerque2001@gmail.com>
2024-12-17 10:52:16 -0300

    feat: update

--- 05-nestJs/test/factories/make-question-attachment.ts
+++ 05-nestJs/test/factories/make-question-attachment.ts
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { QuestionAttachment, QuestionAttachmentProps } from '@/domain/forum/enterprise/entities/question-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionAttachment(override: Partial<QuestionAttachmentProps> = {},
     id?: UniqueEntityID,
@@ -14,4 +16,23 @@ export function makeQuestionAttachment(override: Partial<QuestionAttachmentProps
     )
 
     return questionattachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAttachment(data: Partial<QuestionAttachmentProps> = {}): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+        where: {
+            id: questionAttachment.attachment.toString(),
+        },
+        data: {
+            questionId: questionAttachment.questionId.toString(),
+        }
+    })
+    return questionAttachment
+  }
 }
