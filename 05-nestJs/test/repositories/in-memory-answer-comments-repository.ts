432fc1040f9d896ae09e0068c5609350bbba8b7c
Andre <andrealbuquerque2001@gmail.com>
2024-12-17 10:52:16 -0300

    feat: update

--- 05-nestJs/test/repositories/in-memory-answer-comments-repository.ts
+++ 05-nestJs/test/repositories/in-memory-answer-comments-repository.ts
@@ -2,29 +2,67 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { InMemoryStudentRepository } from './in-memory-student-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
-export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
-    public items: AnswerComment[] = []
-    async findById(id: string) {
-        const answerComment = this.items.find(item => item.id.toString() === id)
-        if (!answerComment) return null
-        return answerComment
-    }
+export class InMemoryAnswerCommentsRepository
+  implements AnswerCommentsRepository
+{
+  public items: AnswerComment[] = []
 
-    async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
-        const answersComment = this.items
-            .filter(item => item.answerId.toString() === answerId)
-            .slice((page - 1) * 20, page * 20)
+  constructor(private studentsRepository: InMemoryStudentRepository) {}
 
-        return answersComment
-    }
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+    if (!answerComment) return null
+    return answerComment
+  }
 
-    async create(answerComment: AnswerComment) {
-        this.items.push(answerComment)
-    }
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answersComment = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
 
-    async delete(answerComment: AnswerComment) {
-        const itemIndex = this.items.findIndex(item => item.id === answerComment.id)
-        this.items.splice(itemIndex, 1)
-    }
+    return answersComment
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComment = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(`Student with id ${comment.authorId} not found`)
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
+
+    return answerComment
+  }
+
+  async create(answerComment: AnswerComment) {
+    this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+    this.items.splice(itemIndex, 1)
+  }
 }
