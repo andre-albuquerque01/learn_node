4a3e6f0a536a4fb5f7fa78a9a1f6e4fe068b822f
Andre <andrealbuquerque2001@gmail.com>
2024-12-19 11:05:52 -0300

    feat: create

---
+++ 05-nestJs/src/infra/http/presenters/attachment-presenter.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { Attachment } from "@/domain/forum/enterprise/entities/attachment";
+
+export class AttachmentPresenter {
+  static toHTTP(attachment: Attachment) {
+    return {
+      id: attachment.id.toString(),
+      url: attachment.url,
+      title: attachment.title,
+    }
+  }
+}
