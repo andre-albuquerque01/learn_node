a95dae105c67f2a5857005cb860bc2030ed1b68f
Andre <andrealbuquerque2001@gmail.com>
2024-12-20 09:55:02 -0300

    feat: create

---
+++ 05-nestJs/src/infra/cache/cache-repository.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export abstract class CacheRepository {
+  abstract set(key: string, value: string): Promise<void>
+  abstract get(key: string): Promise<string | null>
+  abstract delete(key: string): Promise<void>
+}
