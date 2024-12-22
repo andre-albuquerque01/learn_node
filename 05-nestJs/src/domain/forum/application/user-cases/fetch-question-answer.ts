35b64d92e6a8b5f48eefe44b0ecfdf504786ed6d
Andre <andrealbuquerque2001@gmail.com>
2024-12-22 09:03:25 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/user-cases/fetch-question-answer.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { Either, right } from '@/core/either'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Injectable } from '@nestjs/common'
+
+interface FetchQuestionAnswerUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+type FetchQuestionAnswerUseCaseResponse= Either<null, {
+  answers: Answer[]
+}>
+
+@Injectable()
+export class FetchQuestionAnswerUseCase {
+  constructor(private answersRepository: AnswersRepository) { }
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswerUseCaseRequest): Promise<FetchQuestionAnswerUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(questionId, { page })
+
+    return right({ answers })
+  }
+}
