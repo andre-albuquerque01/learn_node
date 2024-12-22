ded64d450a1b2a6744e4e872be37b272b552dc0d
Andre <andrealbuquerque2001@gmail.com>
2024-12-22 09:03:25 -0300

    feat: create

---
+++ 05-nestJs/src/domain/forum/application/user-cases/fetch-question-answer.spec.ts
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import { expect, it, beforeEach } from 'vitest'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { FetchQuestionAnswerUseCase } from './fetch-question-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswersAttachmentRepository } from 'test/repositories/in-memory-answer-attachment-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerAttachmentRepository: InMemoryAnswersAttachmentRepository
+let sut: FetchQuestionAnswerUseCase
+
+describe('Fetch question answers', () => {
+    beforeEach(() => {
+        inMemoryAnswerAttachmentRepository = new InMemoryAnswersAttachmentRepository()
+        inMemoryAnswersRepository = new InMemoryAnswersRepository(inMemoryAnswerAttachmentRepository)
+        sut = new FetchQuestionAnswerUseCase(inMemoryAnswersRepository)
+    })
+
+    it('should be able to fecth question answers', async () => {
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+
+        const result = await sut.execute({
+            questionId: '1',
+            page: 1
+        })
+
+        expect(result.value?.answers).toHaveLength(3)
+    })
+
+    it('should be able to fecth paginated question answers', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswersRepository.create(makeAnswer({ questionId: new UniqueEntityID('1')}))
+        }
+
+        const result = await sut.execute({
+            questionId: '1',
+            page: 2
+        })
+
+        expect(result.value?.answers).toHaveLength(2)
+    })
+})
