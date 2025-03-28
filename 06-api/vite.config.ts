837ba8b92cafe193b3fcd2151626adff6298b40c
andre <andrealbuquerque2001@gmail.com>
2025-03-28 11:20:07 -0300

    feat: create

---
+++ 06-api/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      ['src/http/controllers/**', 'prisma']
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
