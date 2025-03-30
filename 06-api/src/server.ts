6eb7bdefc47695073396ec1c83b8f8bf0d90aaab
andre <andrealbuquerque2001@gmail.com>
2025-03-30 19:36:28 -0300

    feat: create

---
+++ 06-api/src/server.ts
@@ -0,0 +1,13 @@
+import { app } from "./app";
+import { env } from "./env";
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+}).then(() => {
+    console.log(`Server is running`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
