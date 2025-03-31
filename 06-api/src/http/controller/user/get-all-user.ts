8c91623de630dbb09657d64e9f98237051fc585f
andre <andrealbuquerque2001@gmail.com>
2025-03-31 13:57:14 -0300

    feat: create

---
+++ 06-api/src/http/controller/user/get-all-user.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+import { makeUserGetAllFactory } from "@/factory/user/get-all-factory"
+
+export async function getAllUsers(request: FastifyRequest, reply: FastifyReply) {
+    try {
+        const getAllUserService = makeUserGetAllFactory()
+
+        const user = await getAllUserService.execute()
+        reply.status(200).send(user)
+    } catch (err) {
+        reply.status(400).send({ message: 'Error' })
+    }
+}
