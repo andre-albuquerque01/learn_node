d8ce3109da21301f3891c1548986cf35de9136da
andre <andrealbuquerque2001@gmail.com>
2025-04-01 12:41:41 -0300

    feat: update

--- 06-api/src/factory/user/get-all-factory.ts
+++ 06-api/src/factory/user/get-all-factory.ts
@@ -1,5 +1,5 @@
 import { UserPrisma } from "../../repository/prisma/user-prisma";
-import { GetAllUser } from "../../service/user/get-all-users";
+import { GetAllUser } from "../../service/user/get-all-users-service";
 
 export function makeUserGetAllFactory() {
     const userRepository = new UserPrisma()
