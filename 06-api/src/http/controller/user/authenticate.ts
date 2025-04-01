eb11777e243af46f34147692819534b51b98ad93
andre <andrealbuquerque2001@gmail.com>
2025-04-01 12:41:34 -0300

    feat: create

---
+++ 06-api/src/http/controller/user/authenticate.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeUserAuthenticationFactory } from "@/factory/user/authentication-factory";
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+    const authenticateSchema = z.object({
+        email: z.string().email(),
+        password: z
+            .string()
+            .min(8, { message: "A senha deve ter pelo menos 8 caracteres." })
+            .regex(/[A-Z]/, { message: "A senha deve conter pelo menos uma letra maiúscula." })
+            .regex(/[0-9]/, { message: "A senha deve conter pelo menos um número." })
+            .regex(/[^A-Za-z0-9]/, { message: "A senha deve conter pelo menos um caractere especial." }),
+    })
+
+    const data = authenticateSchema.parse(request.body)
+
+    try {
+        const authenticateService = makeUserAuthenticationFactory()
+
+        const { user } = await authenticateService.execute(data)
+
+        const token = await reply.jwtSign({}, {
+            sign: {
+                sub: user.id
+            },
+        })
+
+        return reply.status(201).send({ token })
+    } catch (error) {
+        return reply.status(400).send({
+            error: error instanceof Error ? error.message : "Erro desconhecido.",
+        });
+    }
+}
