d18cc00c8fba1defe3e51531f41348046541099f
andre <andrealbuquerque2001@gmail.com>
2025-04-04 11:16:57 -0300

    feat: update

--- 06-api/src/http/controller/user/routes.ts
+++ 06-api/src/http/controller/user/routes.ts
@@ -2,6 +2,11 @@ import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { getAllUsers } from "./get-all-user";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { authenticate } from "./authenticate";
+import { update } from "./update";
+import { findByIdUser } from "./find-by-id-user";
+import { getByIdUser } from "./get-by-id-user";
+import { deleteByIdUser } from "./delete-by-id-user";
 
 export async function userRoutes(app: FastifyInstance) {
     app.get('/users', async (req, res) => {
@@ -9,5 +14,10 @@ export async function userRoutes(app: FastifyInstance) {
     })
 
     app.post('/register', register)
+    app.post('/sessions', authenticate)
+    app.put('/update', { onRequest: [verifyJWT] }, update)
     app.get('/getAll', { onRequest: [verifyJWT] }, getAllUsers)
+    app.get('/getAllById/:id', { onRequest: [verifyJWT] }, getByIdUser)
+    app.get('/profile', { onRequest: [verifyJWT] }, findByIdUser)
+    app.delete('/delete', { onRequest: [verifyJWT] }, deleteByIdUser)
 }
