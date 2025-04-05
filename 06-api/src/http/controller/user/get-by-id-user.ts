b9236411434ef8ddd397e2855564c38675825761
Andre <andrealbuquerque2001@gmail.com>
2025-04-05 19:51:48 -0300

    feat: update

---
+++ 06-api/src/http/controller/user/get-by-id-user.ts
@@ -0,0 +1,16 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { makeUserFindAllUserFactory } from "@/factory/user/find-by-user-factory";
+
+export async function getByIdUser(request: FastifyRequest, reply: FastifyReply) {
+    try {
+        const findByIdUserService = makeUserFindAllUserFactory()
+
+        const user = await findByIdUserService.execute({ id: request.user.sub })
+
+        return reply.status(200).send(user)
+    } catch (error) {
+        return reply.status(400).send({
+            error: error instanceof Error ? error.message : "Erro desconhecido.",
+        });
+    }
+}
