f95202f522e84591e968fa74082a981d36759851
andre <andrealbuquerque2001@gmail.com>
2025-04-09 14:17:26 -0300

    feat: update

--- 06-api/src/utils/test/create-and-authenticate-user.ts
+++ 06-api/src/utils/test/create-and-authenticate-user.ts
@@ -1,23 +1,24 @@
 import { prisma } from "@/lib/prisma"
 import { hash } from "bcryptjs"
-import { randomBytes } from "crypto"
 import { FastifyInstance } from "fastify"
+import { randomUUID } from "node:crypto"
 import request from "supertest"
 
 export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
-    const randm = randomBytes(10)
+    const email = `user-${randomUUID()}@example.com`
+
     await prisma.user.create({
         data: {
             name: 'John Doe',
-            email: `johndoe${randm}@example.com`,
-            password: await hash('1234567', 6),
+            email,
+            password: await hash('passwordTesteTeste1223@', 6),
         },
     })
     const authResponse = await request(app.server)
         .post('/sessions')
         .send({
-            email: `johndoe${randm}@example.com`,
-            password: '1234567',
+            email,
+            password: 'passwordTesteTeste1223@',
         })
 
     const { token } = authResponse.body
